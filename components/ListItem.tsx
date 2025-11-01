58bbd24658fef298f52e01329275cd68dd0a1dda
SOMASEKAR17 <somasekarnaidu79@gmail.com>
2025-11-02 01:27:58 +0530

    done with the base layout

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
+import { FaPlay } from "react-icons/fa";
 
 interface ListItemProps{
     image:string;
@@ -16,7 +17,7 @@ const ListItem:React.FC<ListItemProps> = ({image,name,href}) => {
         router.push(href);
     }
     return ( 
-        <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+        <button onClick={onClick} className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
             <div className="relative min-h-16 min-w-16">
                 <Image className="object-cover"
                 fill
@@ -26,7 +27,9 @@ const ListItem:React.FC<ListItemProps> = ({image,name,href}) => {
             <p className="font-medium truncate py-5">
                 {name}
             </p>
-            <div className=""></div>
+            <div className="absolute transition opacity-0 rounded-full items-center justify-center bg-green-500 p-4 drop-shadow-md right-5 group-hover:opacity-100 hover:scale-110">
+                <FaPlay className="text-black"/>
+            </div>
         </button>
     );
 }
