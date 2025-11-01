19666204554fbc0ce722f3c004085ccb91c891f0
SOMASEKAR17 <somasekarnaidu79@gmail.com>
2025-11-01 22:40:24 +0530

    first commit , project setup

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css"
+import Sidebar from '@/components/SideBar';
+
+const font = Figtree({subsets:['latin']})
+
+export const metadata: Metadata = {
+  title: "Spotify Clone",
+  description: "Listen to music!",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={font.className}
+        suppressHydrationWarning={true}
+      >
+        <Sidebar>
+        {children}
+        </Sidebar>
+      </body>
+    </html>
+  );
+}
